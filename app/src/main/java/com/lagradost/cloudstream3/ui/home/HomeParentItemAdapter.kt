4826f222d4f0b55a35b63e3c5b302834c92ccd94
Radheshyam Kumar <qaengineer9126@gmail.com>
2026-07-11 12:40:05 +0530

    Fixed home page section expansion issue

--- app/src/main/java/com/lagradost/cloudstream3/ui/home/HomeParentItemAdapter.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/home/HomeParentItemAdapter.kt
@@ -82,7 +82,16 @@ open class ParentItemAdapter(
     ) {
         val binding = holder.view
         if (binding !is HomepageParentBinding) return
-        (binding.homeChildRecyclerview.adapter as? HomeChildItemAdapter)?.submitList(item.list.list)
+        val info = item.list
+        (binding.homeChildRecyclerview.adapter as? HomeChildItemAdapter)?.apply {
+            hasNext = item.hasNext
+            submitList(info.list)
+        }
+        if (isLayout(PHONE)) {
+            binding.homeChildMoreInfo.setOnClickListener {
+                moreInfoClickCallback.invoke(item)
+            }
+        }
     }
 
     override fun onBindContent(
